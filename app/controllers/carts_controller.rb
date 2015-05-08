f2675d75999324b5a7c0034445bed959f5065e9c
Whien <sal95610@gmail.com>
2015-05-08 12:52:00 +0800

    完成取消單一筆商品, todo: destroy_carts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
 # 結帳流程說明 [checkout]: show & edit cart_item & Build shipping_info
 	before_action :login_required
+  respond_to :js, :html
 	# get cart/checkout
 	def checkout
 		#確認這台車的所有商品
@@ -9,4 +10,31 @@ class CartsController < ApplicationController
 		@order = current_user.orders.build
 		@info = @order.order_infos.build
 	end
+
+  def delete_one
+    @cart = Cart.find_by(id: current_cart.id)
+    @product = Product.find(params[:product_id])
+    @cis = @cart.cart_items
+
+    @ci = @cis.find_by(product_id: params[:product_id])
+    if @ci.present?
+      @ci.qty = @ci.qty - 1
+      if @ci.qty == 0
+        @ci.destroy
+      else
+        @ci.update_attributes(product: @product)
+      end
+    else
+      @ci = @cart.cart_items.build(product: @product)
+      @ci.save
+    end
+  end
+
+  def destroy_carts
+    @cart = Cart.find_by(id: current_cart.id)
+    @product = Product.find(params[:product_id])
+    @cis = @cart.cart_items
+    @cis.destroy
+  end
+
 end
