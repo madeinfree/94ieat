d0ae9f1a46d5eb9d032f1fb7ffcb800935e48964
Whien <sal95610@gmail.com>
2015-05-08 20:48:59 +0800

    修正 adm locals返回頁面

--- app/controllers/spadm/locals_controller.rb
+++ app/controllers/spadm/locals_controller.rb
@@ -1,10 +1,10 @@
 class Spadm::LocalsController < Spadm::BaseController
 	def index
-		@locals = Local.all
-    @hash = Gmaps4rails.build_markers(@locals) do |local, marker|
-      marker.lat local.latitude
-      marker.lng local.longitude
-    end
+#		@locals = Local.all
+#    @hash = Gmaps4rails.build_markers(@locals) do |local, marker|
+#      marker.lat local.latitude
+#      marker.lng local.longitude
+#    end
 	end
 	def new
 		@local = Local.new
@@ -12,7 +12,7 @@ class Spadm::LocalsController < Spadm::BaseController
 	def create
 		@local = Local.new(local_params)
 		if @local.save
-			redirect_to locals_url
+			redirect_to spadm_locals_url
 		else
 			render :new
 		end
