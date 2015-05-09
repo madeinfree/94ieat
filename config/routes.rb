4208b0d592359941c0dfd17d62e89f994bed4bfa
Whien <sal95610@gmail.com>
2015-05-10 02:57:23 +0800

    增加route共同檔案內容

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]  
   #靜態頁面
   root 'stores#index'
   get 'about', to: 'pages#about', as: :about
