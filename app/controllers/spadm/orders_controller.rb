05d6854e43f49a1da127080094e1dff1ecc5f143
Whien <sal95610@gmail.com>
2015-05-10 02:58:12 +0800

    order controller 增加 edit 和 update

--- app/controllers/spadm/orders_controller.rb
+++ app/controllers/spadm/orders_controller.rb
@@ -1,11 +1,27 @@
 class Spadm::OrdersController < Spadm::BaseController
 
+  skip_before_filter :verify_authenticity_token, :only => :update
+
   def index
     @orders = Order.all
   end
 
   def edit
-    
+    @order = Order.find(params[:id])
+    respond_to do |format|
+      format.js { @order }
+    end
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_status_params)
+    @order.save
+    render :json => @order
+  end
+
+  def order_status_params
+    params.permit(:payment_method)
   end
 
 end
